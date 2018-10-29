b3ec8cba1a7029f359e89825aa9f0f30aa0cca4e
Sagit <ourfor@foxmail.com>
2018-10-29 20:01:50 +0800

    Update main.java
    update array

--- unit2/main.java
+++ unit2/main.java
@@ -24,15 +24,39 @@ public class main {
         String[] hobbys={"sports","game","movie"};
         System.out.println("循环输出数组中的元素：");
         for(int i=0;i<hobbys.length;i++) System.out.println("数组中第"+(i+1)+"个元素的值为"+hobbys[i]);
-        //for(String str:hobbys) System.out.println(""+str);
+
+        int[] nums=new int[] {61,23,4,74,13,148,20};
+        int max_nums=0;
+        int min_nums=nums[0];
+        double average_nums=0;
+        for(int i:nums){
+            max_nums=i>max_nums?i:max_nums;
+            min_nums=i<min_nums?i:min_nums;
+            average_nums+=i;
+        }
+
+        average_nums/=nums.length;
+
+        System.out.println("数组元素的最大值为："+max_nums);
+        System.out.println("数组元素的最小值为："+min_nums);
+        System.out.println("数组元素的平均值为："+average_nums);
     }
 }
 /*
-维度
-速度快
-开销小
-数组越界
-内存管理
+- 数组名
+- 数组维度
+
+Java中的数组：
+> 静态数组的优点：
+- 速度快
+- 开销小
+- 数组越界
+- 内存管理
+
+
 
 预分配
+与C语言的异同
+增强的for循环
+
  */
