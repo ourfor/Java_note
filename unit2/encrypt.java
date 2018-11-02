7dbec4dc460dbf794851588960922055cf6dd971
Sagit <ourfor@foxmail.com>
2018-11-02 14:09:12 +0800

    Create encrypt.java
    add encrypt information

---
+++ unit2/encrypt.java
@@ -0,0 +1,12 @@
+/*
+ *加密
+ *
+ */
+ 
+public class encrypt {
+    public static void main(String args[]){
+        char encrypt[]="encrypt";
+        System.out.print("输入加密后的内容：");
+        String 
+    }
+}
